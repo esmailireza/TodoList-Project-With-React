ca23b62f55ac308a30b42a4372a3cea885ba77bc
Your Name <you@example.com>
2021-12-04 09:08:39 +0330

    added TodoList file to project

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,5 +1,34 @@
-const TodoList = () => {
-  return <div>todolist</div>;
+import { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+const TodoList = ({ todos, onComplete, onDelete, onUpdateTodo }) => {
+  const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
+
+  const editTodo = (newValue) => {
+    onUpdateTodo(edit.id, newValue);
+    setEdit({ id: null, text: "" });
+  };
+
+  if (todos.length === 0) return <p>add some todos</p>;
+  return (
+    <div>
+      {edit.id ? (
+        <TodoForm submitTodo={editTodo} edit={edit} />
+      ) : (
+        todos.map((todo) => {
+          return (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              onComplete={() => onComplete(todo.id)}
+              onDelete={() => onDelete(todo.id)}
+              onEdit={() => setEdit(todo)}
+            />
+          );
+        })
+      )}
+    </div>
+  );
 };
 
 export default TodoList;
